c62090fa7141b1308f3d7bc0982f246000e32172
tendertree <tendertree@gmail.com>
2024-04-09 15:38:17 +0900

    feat: ui workspaca setupe

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     extends: ["@repo/eslint-config/next.js"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: true,
+ //       project: true,
+		// project: './tsconfig.json',
     },
 
 }
--- packages/ui/app/layout.tsx
+++ packages/ui/app/layout.tsx
@@ -1,11 +1,25 @@
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	)
+import "@repo/ui/global.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@repo/ui/utils"
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head />
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased",
+                    fontSans.variable
+                )}
+            >
+                {children}
+            </body>
+        </html>
+    )
 }
+
