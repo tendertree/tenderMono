a401e4e1a387eb0e41d810b1c5032d94e10975a9
tendertree <tendertree@gmail.com>
2024-04-12 00:15:39 +0900

    fix: tailwind apply @ui when loaded @app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,8 +4,8 @@ import { MainScene } from "@3d/src/babylon/mainScene"
 export default function Page(): JSX.Element {
     return (
         <main>
-            <div className="bg-red-50">
-                This is the main scene
+            <div className="bg-red-50 ">
+                This is the main scene?
                 <MainScene></MainScene>
                 <Profile></Profile>
                 <User />
