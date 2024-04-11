fa6d59f96369477ad4a8f498a6633b92475e6831
tendertree <tendertree@gmail.com>
2024-04-11 17:54:27 +0900

    todo: resolve tailwind effect apply correctlyk

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,15 @@
+import "./globals.css"
 import { Profile, User } from "@ui/src/comp";
 import { MainScene } from "@3d/src/babylon/mainScene"
-import styles from "./page.module.css";
 export default function Page(): JSX.Element {
     return (
-        <main className={styles.main}>
-            This is the main scene
-            <MainScene></MainScene>
-            <Profile></Profile>
-            <User />
-
+        <main>
+            <div className="bg-red-50">
+                This is the main scene
+                <MainScene></MainScene>
+                <Profile></Profile>
+                <User />
+            </div>
         </main>
     );
 }
