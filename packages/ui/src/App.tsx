e909af1196d4d09b67754fb41dafaa8c21b2db0e
tendertree <tendertree@gmail.com>
2024-04-15 13:54:58 +0900

    fix: tailwind.config.ts require tailwind-animate

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,12 +1,9 @@
-import { User } from './comp'
 import './global.css'
 
 function App() {
 
     return (
-        <div className='bg-orange-100'>
-            This is current ui labs ..
-            <User></User>
+        <div className=''>
         </div>
     )
 }
