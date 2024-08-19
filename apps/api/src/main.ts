4b7eff6b0ad9e18b393a36061ec1b03f183a4d20
tendertree <tendertree@gmail.com>
2024-08-20 00:08:48 +0900

    feat: add custom header to prevent from cors conflicts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: process.env.FRONTEND_URL, // Add your frontend URL here 
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Cookie'],
+        credentials: true,
+    });
     await app.listen(3000);
 }
 bootstrap();
