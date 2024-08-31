41bcf3efbd1a0308a3ea4562def4efb812934a42
tendertree <tendertree@gmail.com>
2024-08-31 21:23:28 +0900

    feat: add vitest

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,1 +1,21 @@
-module.exports = require("@config/tailwind-config/tailwind-shadcn");
+
+const baseConfig = require("@config/tailwind-config/tailwind-shadcn");
+
+module.exports = {
+    ...baseConfig,
+    theme: {
+        ...baseConfig.theme,
+        extend: {
+            ...baseConfig.theme.extend,
+            colors: {
+                ...baseConfig.theme.extend.colors,
+                main: {
+                    100: '#4287f5',
+                },
+                side: {
+                    100: '#c1ecf5',
+                },
+            },
+        },
+    },
+};
