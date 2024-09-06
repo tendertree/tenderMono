bd759f5e80c61d50d694132026a96716d9fe8a38
tendertree <tendertree@gmail.com>
2024-09-06 17:38:19 +0900

    fix: module not found

--- labs/auth-next/app/login/page.tsx
+++ labs/auth-next/app/login/page.tsx
@@ -1,17 +1,9 @@
-// import { signin, signup } from "@entity/user/actions/userAction";
 import Login from "@ui/shadcn/feature/Login";
-export default function LoginPage() {
+
+export default function Page(): JSX.Element {
     return (
-
-        <Login />
-
-        // <form>
-        //     <label htmlFor="email">Email:</label>
-        //     <input id="email" name="email" type="email" required />
-        //     <label htmlFor="password">Password:</label>
-        //     <input id="password" name="password" type="password" required />
-        //     <button formAction={signin}>Log in</button>
-        //     <button formAction={signup}>Sign up</button>
-        // </form>
+        <div>data is not
+            <Login />
+        </div>
     )
 }
--- labs/auth-next/middleware.ts
+++ labs/auth-next/middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from './src/server/middleware'
+import { updateSession } from '@src/server/middleware'
 
 export async function middleware(request: NextRequest) {
     return await updateSession(request)
